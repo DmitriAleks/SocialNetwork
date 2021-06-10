475536f0374af88b5ac9829958a9261c8d9fc577
dima <Dima.aleks.94@mail.ru>
2021-06-10 23:55:41 +0300

    add Profileinfo

--- Network/src/Components/Profile/ProfileInfo/Profileinfo.tsx
+++ Network/src/Components/Profile/ProfileInfo/Profileinfo.tsx
@@ -2,9 +2,25 @@ import React from "react";
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 
+type ProfileInfoType = {
+    profile: any
+}
 
-const ProfileInfo = () => {
 
+const ProfileInfo = (props:ProfileInfoType) => {
+    if(!props.profile){
+        return <Preloader/>
+    }
+
+    return (
+        <div>
+            <img
+                src='https://i.ibb.co/M6N3CWk/unnamed.jpg'className={s.img}/>
+            <div className={s.descriptionBlock}>
+                Avatar + description</div>
+
+        </div>
+    )
 }
 
 export default ProfileInfo;
