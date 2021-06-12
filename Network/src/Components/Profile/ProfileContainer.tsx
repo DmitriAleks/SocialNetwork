1a3db75455f5e1611b571d56026dd8d19ad8e12e
dima <Dima.aleks.94@mail.ru>
2021-06-12 23:55:17 +0300

    refactor code, no lesson 60

--- Network/src/Components/Profile/ProfileContainer.tsx
+++ Network/src/Components/Profile/ProfileContainer.tsx
@@ -4,47 +4,44 @@ import axios from "axios";
 import {connect} from "react-redux";
 import {ProfileUserType, setUserProfile} from "../../redux/profile-reducer";
 import {AppStateType} from "../../redux/redux-store";
-import {InitialStateUsersType, UsersType} from "../../redux/users-reducer";
-import {RouteComponentProps, withRouter } from 'react-router-dom';
+import {RouteComponentProps, withRouter} from 'react-router-dom';
 
-type PathParamsType = {
-    userId:string;
-}
-type PropsType = RouteComponentProps<PathParamsType> & UsersProfilePropsType
-
-class ProfileContainer extends React.Component<any> {
-
-    componentDidMount() {
-        debugger
-        let userId = this.props.match.params.userId;
-        // if(!userId){
-        //     userId=2;
-        // }
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/`+userId )
-            .then(response => {
-                this.props.setUserProfile(response.data)
-            });
-    }
-
-    render(){
-        return (
-            <Profile profile={this.props.profile}/>
-        )
-    }
-
-}
-export type UsersProfilePropsType = MapStateToPropsType & MapDispatchToPropsType
 type MapStateToPropsType = {
     profile: ProfileUserType
 }
 type MapDispatchToPropsType = {
     setUserProfile: (users: Array<ProfileUserType>) => void
 }
+type PathParamsType = {
+    userId: string;
+}
+type PropsType = RouteComponentProps<PathParamsType> & UsersProfilePropsType
+export type UsersProfilePropsType = MapStateToPropsType & MapDispatchToPropsType
 
-let mapStateToProps = (state: AppStateType):MapStateToPropsType => ({
+class ProfileContainer extends React.Component<any> {
+
+    componentDidMount() {
+        debugger
+        let userId = this.props.match.params.userId;
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
+            .then(response => {
+                this.props.setUserProfile(response.data)
+            });
+    }
+
+    render() {
+        return (
+            <Profile profile={this.props.profile}/>
+        )
+    }
+
+}
+
+
+let mapStateToProps = (state: AppStateType): MapStateToPropsType => ({
     profile: state.profilePage.profile
 })
 let WithUrlDataContainerComponent = withRouter(ProfileContainer)
 
 
-export default connect (mapStateToProps,{setUserProfile})(WithUrlDataContainerComponent);
+export default connect(mapStateToProps, {setUserProfile})(WithUrlDataContainerComponent);
