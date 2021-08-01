637b4d1a932910a6791347185fcd5458269e0b35
dima <Dima.aleks.94@mail.ru>
2021-08-01 22:36:35 +0300

    completed component ProfileStatus, full lesson 71

--- Network/src/Components/Profile/ProfileInfo/ProfileStatus.tsx
+++ Network/src/Components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -9,19 +9,27 @@ class ProfileStatus extends React.Component<ProfileStatusType, any> {
     state = {
         editMode: false
     }
-
-    
+    activateEditMode ()  {
+        this.setState({
+            editMode : true
+        })
+    }
+    deActivateEditMode ()  {
+        this.setState({
+            editMode : false
+        })
+    }
     render() {
         const {status} = this.props
         return (
             <>
                 {!this.state.editMode &&
                 <div>
-                    <span>{status}</span>
+                    <span onDoubleClick={this.activateEditMode.bind(this)}>{status}</span>
                 </div>}
                 {this.state.editMode &&
                 <div>
-                    <input value={status}/>
+                    <input value={status} onBlur={this.deActivateEditMode.bind(this)} autoFocus={true}/>
                 </div>}
 
             </>
