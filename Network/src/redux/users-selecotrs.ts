a9fa51340aef9e7fc169751ca8f408fd9a378eb8
dima <Dima.aleks.94@mail.ru>
2021-09-05 17:57:32 +0300

    add second dependencies

--- Network/src/redux/users-selecotrs.ts
+++ Network/src/redux/users-selecotrs.ts
@@ -5,15 +5,15 @@ import {AppStateType} from "./redux-store";
 export const getUsersTwo = (state: AppStateType) => {//примитивный селектор
     return state.usersPage.users;
 }
-export const getUserSelectorTwo = (state: AppStateType) => {
-    getUsersTwo(state).filter(u=> true)
-}
 export const getUsersSuperSelectorTwo = createSelector(getUsersTwo, (users)=>{// сложный селектор, использует
     //примитивный getUsersTwo для получения необходимых данных (users), при изменение значения в users функцию возврщает нам
     // новое значение
  return   users.filter(u=> true);
 })
-
+// export const getUsersSuperSelectorTwo = createSelector(getUsersTwo,getIsFetching, (users,isFetching)=>{
+// При наличии более одной зависимости, остальные добавляются через запятую (простые селекторы и значение)
+//     return   users.filter(u=> true);
+// })
 
 export const getUsers = (state: AppStateType) => {
     return state.usersPage;
