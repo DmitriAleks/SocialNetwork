e6cf7d8ced5780d11a2e4a97d83a618f10ae4550
dima <Dima.aleks.94@mail.ru>
2021-09-06 22:38:23 +0300

    add preloder

--- Network/src/redux/auth-reducer.tsx
+++ Network/src/redux/auth-reducer.tsx
@@ -63,3 +63,21 @@ export const logout = () => (dispatch: ThunkDispatch<{}, {}, AppActionsType>) =>
     });
 }
 
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+
+const initialState = {
+    status: 'loading' as RequestStatusType
+}
+
+type InitialStateType = typeof initialState
+
+export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'APP/SET-STATUS':
+            return {...state, status: action.status}
+        default:
+            return state
+    }
+}
+
+type ActionsType = any
