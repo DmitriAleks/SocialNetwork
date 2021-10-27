fd704a4cd1da97ae713113d68db5e87663f7ca50
DmitriAleks <Dima.aleks.94@mail.ru>
2021-10-27 21:21:53 +0300

    change style for header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { logout } from '../../redux/auth-reducer';
-import s from './Header.module.css';
+import {NavLink} from 'react-router-dom';
+import style from './Header.module.css';
+
 type HeaderType = {
-    login: string|null
+    login: string | null
     isAuth: boolean
-    logout:() => void
+    logout: () => void
 }
 
 
-
 const Header = (props: HeaderType) => {
-    return ( <header className={s.header}>
-            <div>
-                <img src="https://cs6.pikabu.ru/post_img/2017/09/08/6/150486171013913777.gif"/>
-                {props.isAuth
-                    ? <button onClick={props.logout}>{props.login} Покинуть профиль</button> //не работаю div
-                    : <NavLink to={'/login'}>Login</NavLink>}
-
+    return (<header className={style.header}>
+            <div className={style.contentHeader}>
+                <span className={style.title}> Social Network</span>
+                <div className={style.menuLogin}>
+                    {props.isAuth
+                        ? <button onClick={props.logout}>{props.login} Log out </button>
+                        : <button> <NavLink to={'/login'}>Login</NavLink></button>}
+                </div>
             </div>
         </header>
-
     )
 }
 
